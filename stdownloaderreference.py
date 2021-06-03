b1ddde49835227590609217fc17924b921b87b4e
Wei Yuxin <wei.yuxin14@iamtemasek.edu.sg>
2021-06-03 20:45:31 +0800

    updates

--- stdownloaderreference.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-data = open("/Users/weiyushit/OneDrive/Github stuff/teststreamlit/test.xlsx", 'rb').read()
-base64_encoded = base64.b64encode(data).decode('UTF-8')
-
-print(base64_encoded)
-
